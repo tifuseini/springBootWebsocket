db4a75d4fe16116aa68db1da20f1c1a29a7d807f
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-24 22:36:29 +0000

    Web Config

--- src/main/java/com/example/webSocket/WebSocketConfig.java
+++ src/main/java/com/example/webSocket/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.example.webSocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,5 +12,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
     }
 }
