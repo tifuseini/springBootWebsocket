18aafe59878aa8b58ddd5fbaf18f4449d6764455
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-24 22:45:02 +0000

    Greeting Controller

--- src/main/java/com/example/webSocket/GreetingController.java
+++ src/main/java/com/example/webSocket/GreetingController.java
@@ -1,11 +1,13 @@
 package com.example.webSocket;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
 
     public Greeting greet(HelloMessage helloMessage){
+        return new Greeting((HtmlUtils.htmlEscape(helloMessage)))
 
     }
 }
