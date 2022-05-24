33bb11721170886c3bb6d26c4768c4258f3a9dfa
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-24 22:48:18 +0000

    Greeting Controller

--- src/main/java/com/example/webSocket/GreetingController.java
+++ src/main/java/com/example/webSocket/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.util.HtmlUtils;
 public class GreetingController {
 
     @MessageMapping("Hello")
-    @SendTo
+    @SendTo()
     public Greeting greet(HelloMessage helloMessage){
         return new Greeting("Hello " + (HtmlUtils.htmlEscape(helloMessage.getName())));
 
