30b1bd0b93c5863c5cbeb8d16fe9cd91625fba92
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-24 22:33:42 +0000

    Web Config

--- src/main/java/com/example/webSocket/WebSocketConfig.java
+++ src/main/java/com/example/webSocket/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.example.webSocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig {
 }
