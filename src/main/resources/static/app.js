f2e69010f6e80a930f169c070bf8acb25a65806e
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-24 23:24:50 +0000

    Html file

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -13,7 +13,7 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('/stomp-endpoint');
+    var socket = new SockJS('/client-endpoint');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
