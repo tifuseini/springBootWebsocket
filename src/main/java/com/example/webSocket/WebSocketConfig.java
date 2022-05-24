247d8db4ae4fc269cba7fe4709151933be5cd285
tifuseini <tifuseini@st.ug.edu.gh>
2022-05-24 22:41:14 +0000

    Web Config

--- src/main/java/com/example/webSocket/WebSocketConfig.java
+++ src/main/java/com/example/webSocket/WebSocketConfig.java
@@ -17,6 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(final MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
 
     }
 }
